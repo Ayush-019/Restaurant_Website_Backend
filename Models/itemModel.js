29a953e9b6f07355fe3ebd77aeba3a4a1cc87944
Ayush-019 <gargayush2002@gmail.com>
2022-05-27 12:00:41 +0530

    Models

--- Models/itemModel.js
+++ Models/itemModel.js
@@ -1,1 +1,37 @@
 const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Enter Item's name!"],
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: [true, "Enter Item's Category"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Enter Item's Price!"],
+  },
+  itemType: {
+    type: String,
+    required: [true, "Enter Item's type"],
+  },
+
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+});
+
+module.exports = mongoose.model("Item", itemSchema);
---
+++ Models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter your name!"],
+    minLength: [3, "Name must have more than 3 characters"],
+  },
+
+  MobileNo: {
+    type: String,
+    required: [true, "Please Enter your MobileNo.!"],
+    unique: true,
+  },
+
+  TableNo: {
+    type: Number,
+    required: [true, "Please Enter your TableNo.!"],
+  },
+
+});
+
+
+module.exports = mongoose.model("User", userSchema);
